0fc8ebc1694a6675189c31ab52ffb5ed7e023734
wagh.sn@northeastern.edu <wagh.sn@northeastern.edu>
2022-11-02 20:30:11 -0400

    Ductch Auction

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require('solidity-coverage');
+require("@nomiclabs/hardhat-truffle5");
+module.exports = {
+  solidity: "0.8.9",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    }
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
